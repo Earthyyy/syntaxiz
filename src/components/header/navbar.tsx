425c8a45d6fd187f5c3ca5245414e1033a9683ae
Earthyy <elardi2001@gmail.com>
2023-11-27 15:09:45 +0100

    feat: build navbar

---
+++ src/components/header/navbar.tsx
@@ -0,0 +1,21 @@
+import { Button } from "../ui/button";
+import { Play, ListTree, Leaf } from "lucide-react";
+
+const Navbar = () => {
+  return (
+    <nav className="h-[3.75rem] w-full flex justify-center items-center py-3 px-6 border-b ">
+      <div className="flex items-center justify-between w-full">
+        <a href="#" className="flex items-center justify-center gap-1">
+          <Leaf className="w-7 h-7 mr-1 text-[#306844]" />
+          <span className="text-2xl font-normal">syntaxiz</span>
+        </a>
+        <Button className="">
+          <Play className="w-4 h-4 mr-2" />
+          Generate Code
+        </Button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
