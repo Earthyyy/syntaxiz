8830936475e73c3972617f3d0a2045b969930016
Earthyy <elardi2001@gmail.com>
2023-12-04 14:58:49 +0100

    feat: implement the tree generator function

--- src/components/header/navbar.tsx
+++ src/components/header/navbar.tsx
@@ -1,17 +1,46 @@
 "use client";
-import { getOutgoers, useEdges, useNodes, useStoreApi } from "reactflow";
+import {
+  getOutgoers,
+  useEdges,
+  useNodes,
+  useReactFlow,
+  useStoreApi,
+} from "reactflow";
 import { Button } from "../ui/button";
 import { Play, ListTree, Leaf } from "lucide-react";
 import { useCallback } from "react";
+import { type Node } from "reactflow";
+
+type NodeData = {
+  label: string;
+  value: string;
+  children: Node[];
+};
 
 const Navbar = () => {
-  const store = useStoreApi();
+  const store = useReactFlow();
+  const { getNodes, getEdges } = store;
 
   const onClick = useCallback(() => {
-    const { getNodes, edges } = store.getState();
-    console.log(getNodes());
-    console.log(edges);
-  }, [store]);
+    const visited = new Set();
+
+    function generateTree(root: Node<NodeData>): any {
+      if (visited.has(root.id)) {
+        return null;
+      }
+
+      visited.add(root.id);
+
+      const children = getOutgoers(root, getNodes(), getEdges());
+      return {
+        node: root.data.label.toLowerCase(),
+        value: root.data?.value,
+        children: children.map((child) => generateTree(child)).filter(Boolean),
+      };
+    }
+
+    console.log(generateTree(getNodes()[0]));
+  }, [getEdges, getNodes]);
 
   return (
     <nav className="h-[3.75rem] w-full flex justify-center items-center py-3 px-6 border-b ">
