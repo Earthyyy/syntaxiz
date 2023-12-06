cbe8a6ff52463305aa31f7b1e79924c98b59c32e
EL Ardi Anas <93055532+Earthyyy@users.noreply.github.com>
2023-12-06 17:30:42 +0100

    feat: improve ui functionalities (#8)
    * feat: add flow control buttons
    
    * chore: add `@radix-ui/react-icons` to deps
    
    * feat: implement `reset` feature
    
    * feat: add `loading` state to "Generate Code" btn
    
    * feat: add `clip-board` button

---
+++ src/components/ui/clipboard-button.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Check, CopyIcon } from "lucide-react";
+import { FC, useState } from "react";
+
+interface ClipBoardButtonProps {
+  output: any;
+}
+
+const ClipBoardButton: FC<ClipBoardButtonProps> = ({ output }) => {
+  const [isCopying, setIsCopying] = useState(false);
+
+  return (
+    <button
+      className="inline-flex items-center justify-center whitespace-nowrap rounded-[.25rem] text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 z-10 h-6 w-6 text-zinc-50 hover:bg-zinc-700 hover:text-zinc-50 absolute right-4 top-4"
+      onClick={() => {
+        setIsCopying(true);
+        navigator.clipboard.writeText(output);
+        setTimeout(() => {
+          setIsCopying(false);
+        }, 1000);
+      }}
+    >
+      <span className="sr-only">Copy</span>
+      <CopyIcon
+        className={cn(
+          "h-3 w-3 transition-all",
+          isCopying ? "hidden opacity-0" : "block opacity-100"
+        )}
+      />
+
+      <Check
+        className={`h-3 w-3 transition-all ${
+          isCopying ? "block opacity-100" : "hidden opacity-0"
+        }`}
+      />
+    </button>
+  );
+};
+
+export default ClipBoardButton;
