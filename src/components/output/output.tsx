9ab7f76d7fa4136005127e745433a4ba46e71adc
Earthyy <elardi2001@gmail.com>
2023-12-07 21:41:08 +0100

    feat: add `clear` button to reset output result

--- src/components/output/output.tsx
+++ src/components/output/output.tsx
@@ -2,6 +2,7 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import useDataStore from "@/hooks/use-data-store";
 import ClipBoardButton from "@/components/ui/clipboard-button";
 import RunButton from "./run-button";
+import ClearButton from "./clear-button";
 
 const Output = () => {
   const data = useDataStore((state) => state.data);
@@ -9,7 +10,10 @@ const Output = () => {
     <div className="w-1/3 dark:bg-[#09090b] border-l py-4 px-6 flex flex-col gap-2">
       <header className="flex items-center justify-between">
         <h2 className="text-2xl font-semibold">Output</h2>
-        <RunButton />
+        <div className="flex items-center justify-center gap-2">
+          <RunButton />
+          <ClearButton />
+        </div>
       </header>
       <ScrollArea className="border rounded-lg flex-1 p-4 bg-zinc-50 dark:bg-zinc-950 overflow-auto">
         <ClipBoardButton output={data?.response} />
