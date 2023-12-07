6972501b80b096912972d24dab484b3e80b281d7
Earthyy <elardi2001@gmail.com>
2023-12-07 21:17:08 +0100

    fix: fix clipboard icon `bg-color` in light mode

--- src/components/ui/clipboard-button.tsx
+++ src/components/ui/clipboard-button.tsx
@@ -13,7 +13,7 @@ const ClipBoardButton: FC<ClipBoardButtonProps> = ({ output }) => {
 
   return (
     <button
-      className="inline-flex items-center justify-center whitespace-nowrap rounded-[.25rem] text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 z-10 h-6 w-6 text-zinc-50 hover:bg-zinc-700 hover:text-zinc-50 absolute right-4 top-4"
+      className="inline-flex items-center justify-center whitespace-nowrap rounded-[.25rem] text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 z-10 h-6 w-6  absolute right-4 top-4 dark:hover:bg-zinc-700 hover:bg-zinc-200"
       onClick={() => {
         setIsCopying(true);
         navigator.clipboard.writeText(output);
