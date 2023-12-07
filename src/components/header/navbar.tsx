c626a296570062c14cb115546f214935709b2352
Earthyy <elardi2001@gmail.com>
2023-12-07 20:38:40 +0100

    feat: add link to `github` repo

--- src/components/header/navbar.tsx
+++ src/components/header/navbar.tsx
@@ -6,8 +6,8 @@ import {
   useReactFlow,
   useStoreApi,
 } from "reactflow";
-import { Button } from "../ui/button";
-import { Play, ListTree, Leaf, Loader2 } from "lucide-react";
+import { Button, buttonVariants } from "../ui/button";
+import { Play, ListTree, Leaf, Loader2, Github } from "lucide-react";
 import { useCallback } from "react";
 import { type Node } from "reactflow";
 import { useMutation } from "react-query";
@@ -15,6 +15,7 @@ import axios from "axios";
 import useDataStore from "@/hooks/use-data-store";
 import { cn } from "@/lib/utils";
 import { ModeToggle } from "../ui/mode-toggle";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
 type NodeData = {
   label: string;
@@ -65,12 +66,12 @@ const Navbar = () => {
   return (
     <nav className="h-[3.75rem] w-full flex justify-center items-center py-3 px-6 border-b ">
       <div className="flex items-center justify-between w-full">
-        <a href="#" className="flex items-center justify-center gap-1">
-          <Leaf className="w-7 h-7 mr-1 text-[#306844]" />
-          <span className="text-2xl font-normal">syntaxiz</span>
+        <a href="#" className="flex items-center justify-center gap-2">
+          <Leaf className="w-7 h-7 fill-none" />
+          <span className="text-lg font-bold">Syntaxiz</span>
         </a>
 
-        <div className="flex gap-2">
+        <div className="flex gap-2 items-center justify-center">
           <Button
             className=""
             onClick={() => assemblify()}
@@ -87,6 +88,16 @@ const Navbar = () => {
             />
             Generate Code
           </Button>
+          <a
+            href="https://github.com/Earthyyy/syntaxiz"
+            target="_blank"
+            className={buttonVariants({
+              variant: "ghost",
+              className: "rounded-[.5rem] w-8 h-8 relative px-0 py-0",
+            })}
+          >
+            <GitHubLogoIcon className="w-4 h-4" />
+          </a>
           <ModeToggle />
         </div>
       </div>
