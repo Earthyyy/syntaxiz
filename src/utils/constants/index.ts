36ed5200ae855aefc1a1558502b4d675281d83f1
Earthyy <elardi2001@gmail.com>
2023-12-08 22:20:03 +0100

    chore: add `constants` to represent components

---
+++ src/utils/constants/index.ts
@@ -0,0 +1,14 @@
+export const COMPONENTS = {
+  BODY: "body",
+  BIN_OP: "BinOp",
+  BOOL_OP: "BoolOp",
+  ASSIGN: "Assign",
+  COMPARE: "Compare",
+  IF: "If",
+  WHILE: "While",
+  ID: "Id",
+  CONSTANT: "Constant",
+  OP: "Op",
+  TEST: "test",
+  OR_ELSE: "orelse",
+} as const;
