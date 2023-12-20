b022dbdb469a18d754e8911cc8b8005c08cde3d0
Hamza AMIMI <132934834+AmimiHamza@users.noreply.github.com>
2023-12-20 19:59:19 +0100

    fix:structure the code (#28)

--- src/utils/server/index.js
+++ src/utils/server/index.js
@@ -1,15 +1,14 @@
 
 module.exports = {
     translateProgram,
-
 };
 function translateProgram(tree, assemblyCode) {
     translateBody(tree, assemblyCode);
 }
 function translateBody(nodes, assemblyCode) {
-  let childs=nodes["children"]
-  
+  let childs=nodes["children"]  
   const nodeValues = Object.values(childs);
+
   for (let node of nodeValues) {
       translateStatement(node, assemblyCode);
   }
@@ -27,123 +26,96 @@ function translateStatement(node, assemblyCode) {
       }else if (nodeType === "for") {
         translateForLoop(node, assemblyCode);
     }
-      // Add more cases for other statement types if needed
   }
 }
 function translateAssignment(node, assemblyCode) {
-    const idIndex = node["children"][0].node === "id" ? 0 : 1;
-    const id = getnode(node['children'],'id')["value"];
-    const valueNode = node["children"][1-idIndex]
+  const id = getNode(node['children'],'id')["value"]; 
+  const idIndex = node["children"][0].node === "id" ? 0 : 1; 
+  const valueNode = node["children"][1-idIndex] 
+  moveValueIntoRegister(valueNode,'ebx',assemblyCode)
 
-    if (valueNode["node"] === "constant") {
-        const rightOperand = valueNode["value"];
-        const assemblyInstruction = `MOV eax, ${rightOperand}\nMOV [${id}], eax`;
-        assemblyCode.push(assemblyInstruction);
-    } else if (valueNode["node"] === "binop") {
-        binopStatement(valueNode, assemblyCode);
-        let bigId = getnode(node['children'],'id')["value"];
-        const assemblyInstruction = `MOV [${bigId}], ebx`;
-        assemblyCode.push(assemblyInstruction);
-    }else if (valueNode["node"] === "id") {
-        const rightOperand = '['+valueNode["value"]+']';
-        const assemblyInstruction = `MOV eax, ${rightOperand}\nMOV [${id}], eax`;
-        assemblyCode.push(assemblyInstruction);
-}}
-function getnode(children,id){
-  for (let node of children){
-    if (node['node']==id){
-      return node;
-  }}
-
-}
-function translateForLoop(node,assemblyCode){
-  const iter = getnode(node["children"],'iter');
-  const body = getnode(node['children'],'body');
-  const id = getnode(iter['children'],'id')['value'];
-  let start= getnode(iter['children'],'start')['children'][0];
-  let end= getnode(iter['children'],'end')['children'][0];
-  if (start['node']=='id'){
-    start='['+start['value']+']'
-  }else if (start['node']=='constant'){
-    start=start['value']
-  }
-  if (end['node']=='id'){
-    end='['+end['value']+']'
-  }else if (end['node']=='constant'){
-    end=end['value']
-  }
-  const loopStartLabel = 'START_FOR_' + generateLabel();
-  const loopEndLabel = 'END_FOR_' + generateLabel();
-  assemblyCode.push(`MOV eax, ${start}\nMOV [start], eax`);
-  assemblyCode.push(`${loopStartLabel}:`);
-  assemblyCode.push(`MOV eax, [start]\nMOV ebx, ${end}\nCMP eax, ebx\nJUMP_IF_NOT_LESS ${loopEndLabel} `);
-  translateBody(body,assemblyCode)
-  assemblyCode.push(`MOV eax, [start]\nADD eax, 1\nMOV [start], eax\nJUMP ${loopStartLabel}\n${loopEndLabel} `);
-
-}
-
-function translateWhileLoop(node, assemblyCode) {
-  const condition = node["children"][0];
-  const body = node["children"][1];
-
-  const loopStartLabel = 'START_WHILE_' + generateLabel();
-  const loopEndLabel = 'END_WHILE_' + generateLabel();
-
-  assemblyCode.push(`${loopStartLabel}:`);
-
-  if (condition["node"] === "test") {
-    translateCondition(condition, loopEndLabel, assemblyCode);
-  } 
-
-  translateBody(body, assemblyCode);
-
-  assemblyCode.push(`JUMP ${loopStartLabel}`);
-  assemblyCode.push(`${loopEndLabel}:`);
+  //assignement 
+  const assemblyInstruction = `MOV [${id}], ebx`; 
+  assemblyCode.push(assemblyInstruction);
 }
 function translateIfStatement(node, assemblyCode) {
-    const condition =  getnode(node['children'],'test');
+    const condition =  getNode(node['children'],'test');
     const body = node["children"][1];
     const elseBody = node["children"][2];
+
 
     const elseLabel = 'ELSE_'+generateLabel();
     const endIfLabel = 'END_IF_'+generateLabel();
 
-    translateCondition(condition, elseLabel, assemblyCode);
+
+    translateCondition(condition, elseBody ? elseLabel : endIfLabel, assemblyCode);
     translateBody(body, assemblyCode);
-
     assemblyCode.push(`JUMP ${endIfLabel}`);
-    assemblyCode.push(`${elseLabel}:`);
-
     if (elseBody) {
-        translateBody(elseBody, assemblyCode);
+      assemblyCode.push(`${elseLabel}:`);
+      translateBody(elseBody, assemblyCode);
     }
-
     assemblyCode.push(`${endIfLabel}:`);
 }
-function translateCondition(condition, jumpLabel, assemblyCode) {
-      let comparenode=condition["children"][0];
-      let leftOperand = comparenode["children"][0];
-      if (leftOperand['node'] == 'constant') {
-          leftOperand = leftOperand['value'];
-      } else if (leftOperand['node'] == 'id') {
-          leftOperand = '[' + leftOperand['value'] + ']';
-      }
-
-      const operator =getnode(comparenode["children"],'op')['value'];
-      let rightOperand = comparenode["children"][2];
-      if (rightOperand['node'] == 'constant') {
-          assemblyCode.push(`MOV ebx, ${rightOperand['value']}`);
-      } else if (rightOperand['node'] == 'id') {
-          assemblyCode.push(`MOV ebx, [${rightOperand['value']}]`);
-      }else if (rightOperand['node']=='binop'){
-        binopStatement(rightOperand,assemblyCode)
-      }
-      assemblyCode.push(`MOV eax, ${leftOperand}`);
-      assemblyCode.push(`CMP eax, ebx`);
-      assemblyCode.push(`JUMP_IF_NOT_${getJumpInstruction(operator)} ${jumpLabel}`);
-      
+function translateWhileLoop(node, assemblyCode) {
+    const condition = node["children"][0];
+    const body = node["children"][1];
   
-  }
+  
+    const loopStartLabel = 'START_WHILE_' + generateLabel();
+    const loopEndLabel = 'END_WHILE_' + generateLabel();
+  
+  
+    assemblyCode.push(`${loopStartLabel}:`);
+    if (condition["node"] === "test") {
+      translateCondition(condition, loopEndLabel, assemblyCode);
+    } 
+    translateBody(body, assemblyCode);
+    assemblyCode.push(`JUMP ${loopStartLabel}`);
+    assemblyCode.push(`${loopEndLabel}:`);
+}
+function translateForLoop(node,assemblyCode){
+    const iter = getNode(node["children"],'iter');
+    const body = getNode(node['children'],'body');
+    const id = getNode(iter['children'],'id')['value'];
+    let start= getNode(iter['children'],'start')['children'][0];
+    let end= getNode(iter['children'],'end')['children'][0];
+  
+  
+    if (start['node']=='id'){
+      start='['+start['value']+']'
+    }else if (start['node']=='constant'){
+      start=start['value']
+    }
+    if (end['node']=='id'){
+      end='['+end['value']+']'
+    }else if (end['node']=='constant'){
+      end=end['value']
+    }
+  
+  
+    const loopStartLabel = 'START_FOR_' + generateLabel();
+    const loopEndLabel = 'END_FOR_' + generateLabel();
+  
+  
+    assemblyCode.push(`MOV eax, ${start}\nMOV [start], eax`);
+    assemblyCode.push(`${loopStartLabel}:`);
+    assemblyCode.push(`MOV eax, [start]\nMOV ebx, ${end}\nCMP eax, ebx\nJUMP_IF_NOT_LESS ${loopEndLabel} `);
+    translateBody(body,assemblyCode)
+    assemblyCode.push(`MOV eax, [start]\nADD eax, 1\nMOV [start], eax\nJUMP ${loopStartLabel}\n${loopEndLabel} `);
+}
+function translateCondition(condition, jumpLabel, assemblyCode) {
+        let comparenode=condition["children"][0];
+        const operator =getNode(comparenode["children"],'op')['value'];
+        const rightOperand = comparenode["children"][2];
+        const leftOperand = comparenode["children"][0];   
+  
+  
+        moveValueIntoRegister(rightOperand,'ebx',assemblyCode)
+        moveValueIntoRegister(leftOperand,'eax',assemblyCode)
+        assemblyCode.push(`CMP eax, ebx`);
+        assemblyCode.push(`JUMP_IF_NOT_${getJumpInstruction(operator)} ${jumpLabel}`);
+}
 function binopStatement(node, assemblyCode) {
   var binop_counter=0;
   for (let subnode of node['children'] ){
@@ -156,27 +128,24 @@ function binopStatement(node, assemblyCode) {
   }
 
   if(binop_counter==0){ //1er cas de binop simple
-  let op2 = getnode(node['children'],'op')["value"];
+  let op2 = getNode(node['children'],'op')["value"];
   let bop2 = getOperator(op2);
   let filtred=node['children'].filter(item => item['node'] !== 'op');
   let smallId = filtred[0];
-  if (smallId['node']=='constant'){
-    smallId=smallId['value']
-  }else if(smallId['node']=='id'){
-    smallId='['+smallId['value']+']'
-  }
+  moveValueIntoRegister(smallId,'ebx',assemblyCode)
   let right = filtred[1]; 
   if (right['node']=='constant'){
     right=right['value']
   }else if(right['node']=='id'){
     right='['+right['value']+']'
   }
-  const assemblyInstruction=`MOV ebx, ${smallId}\nMOV eax, ${right}\n${bop2} ebx, eax`;
+  moveValueIntoRegister(right,'eax',assemblyCode)
+  const assemblyInstruction=`${bop2} ebx, eax`;
   assemblyCode.push(assemblyInstruction);
 
 }
 else if(binop_counter==1)  { //2eme cas de binop imbriqué 
-  let op2 = getnode(node['children'],'op')["value"];
+  let op2 = getNode(node['children'],'op')["value"];
   let bop2 = getOperator(op2);
   let filtred=node['children'].filter(item => item['node'] !== 'op'&&item['node'] !== 'binop');
   if (filtred[0]['node']=='id'){
@@ -184,12 +153,12 @@ else if(binop_counter==1)  { //2eme cas de binop imbriqué
 }else if (filtred[0]['node']=='constant'){
   var bigId=  filtred[0]['value'];
 }
-  let binode=getnode(node['children'],'binop');
+  let binode=getNode(node['children'],'binop');
     binopStatement(binode, assemblyCode);
     const assemblyInstruction = `MOV eax, ebx\nMOV ebx, ${bigId}\n${bop2} ebx, eax`;
     assemblyCode.push(assemblyInstruction);
 }else if(binop_counter==2){
-  let op = getnode(node['children'],'op')["value"];
+  let op = getNode(node['children'],'op')["value"];
   let bop = getOperator(op);
   let filtred=node['children'].filter(item => item['node'] !== 'op');
   let binode1=filtred[0];
@@ -203,19 +172,38 @@ else if(binop_counter==1)  { //2eme cas de binop imbriqué
 
 }
 }
+function moveValueIntoRegister(node,register,assemblyCode){ 
+  if (node["node"] === "constant") {
+    const Operand = node["value"];
+    assemblyCode.push(`MOV ${register}, ${Operand}`);
+
+  }else if (node["node"] === "binop") {
+    binopStatement(node, assemblyCode);
+    if(register=='eax'){
+      assemblyCode.push(`MOV eax, ebx`);
+    }
+
+    }else if (node["node"] === "id") {
+    const Operand = '['+node["value"]+']';
+    assemblyCode.push(`MOV ${register}, ${Operand}`);
+}
+}
+function getNode(children,type){
+  for (let node of children){
+    if (node['node']==type){
+      return node;
+  }}
+}
 function getOperator(operator) {
-  // Map your custom condition to x86 assembly jump instruction
   let ops = {
       "+": "add",
       "-": "sub",
       "*": "imul",
       "/": "idiv",
-      // Add more mappings as needed
   }[operator];
   return ops;
 }
 function getJumpInstruction(operator) {
-  // Map your custom condition to x86 assembly jump instruction
   const jumpInstruction = {
       "<": "LESS",
       ">": "GREATER",
@@ -228,12 +216,9 @@ function getJumpInstruction(operator) {
       "and": "AND",
       "or": "OR",
 
-      // Add more mappings as needed
   }[operator];
   return jumpInstruction;
 }
-
-
 function generateLabel() {
     if (!generateLabel.counter) {
       generateLabel.counter = 1;
@@ -242,8 +227,4 @@ function generateLabel() {
     }
   
     return `LABEL_${generateLabel.counter}`;
-  }
-
-
-
-
+}
